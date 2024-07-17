8a6a8d4a727456c309383576d96db98175bbd3e1
琉見 <42371341+rumamitw01@users.noreply.github.com>
2024-07-17 14:11:27 +0800

    markdown test

--- js/NotesView.js
+++ js/NotesView.js
@@ -87,7 +87,7 @@ _createListItemHTML(id, title, body, updated) {
 
     updateActiveNote(note) {
         this.root.querySelector(".notes__title").value = note.title;
-        this.root.querySelector(".notes__body").value = marked.parse(note.body);
+        this.root.querySelector(".notes__body").html = note.body;
 
         this.root.querySelectorAll(".notes__list-item").forEach(noteListItem => {
             noteListItem.classList.remove("notes__list-item--selected");
